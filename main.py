c89a65cfd7420daa5284ffe9901023c5adda3d6e
v1kstrand <david.vikstrand@example.com>
2025-07-28 15:48:27 +0200

    f

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import yaml
-
-from qcit.config import set_torch_config
-set_torch_config()
 from qcit.training import start_training
-
 
 with open("/notebooks/params.yaml", "r",  encoding="utf-8") as f:
     args = yaml.safe_load(f)
