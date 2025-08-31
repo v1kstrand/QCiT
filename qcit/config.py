f23d740fbd6a2bd0ebff3ac9aabac5d798b5a1e7
v1kstrand <david.vikstrand@example.com>
2025-08-31 15:53:05 +0200

    f

--- qcit/config.py
+++ qcit/config.py
@@ -160,7 +160,7 @@ def torch_config_v2(safe_config: bool = True):
     ind.cuda.cutlass_max_profiling_configs = None
     ind.cuda.cutlass_backend_min_gemm_size = 32 * 32 * 32
     ind.cuda.cutlass_op_denylist_regex = None
-    print(f"INFO: Torch Config Set ✔✔  Profile={'SAFE' if safe_config else 'UNSAFE/MAX'}")
+    print(f"INFO: Torch Config Set V2 ✔✔  Profile={'SAFE' if safe_config else 'UNSAFE/MAX'}")
 
 def get_args():
     parser = argparse.ArgumentParser()
